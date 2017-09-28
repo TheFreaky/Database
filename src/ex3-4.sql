f7c56fbf6c44b0a29ecac9c598a9bd3de975eb87
TheFreaky <maksoh_35@mail.ru>
2017-09-28 16:30:58 +0300

    Initial commit

---
+++ src/ex3-4.sql
@@ -0,0 +1,67 @@
+CREATE TABLE movies (
+  name        VARCHAR(50),
+  description TEXT,
+  year        SMALLINT CHECK (year > 1900 AND year < date_part('year', CURRENT_DATE) + 10),
+  genres      VARCHAR(50),
+  country     VARCHAR(50),
+  budget      NUMERIC CHECK (budget > 10000)
+);
+
+CREATE TABLE person (
+  surname    VARCHAR(50) UNIQUE,
+  name       VARCHAR(50) UNIQUE,
+  birthday   DATE,
+  motherland VARCHAR(50)
+);
+
+CREATE TABLE actors (
+  number_of_movies INTEGER CHECK (number_of_movies > 5)
+)
+  INHERITS (person);
+
+CREATE TABLE producer (
+  motherland VARCHAR(50) DEFAULT 'USA'
+)
+  INHERITS (person);
+
+ALTER TABLE movies
+  ADD movie_id SERIAL PRIMARY KEY;
+ALTER TABLE actors
+  ADD movie_id INTEGER REFERENCES filmography.public.movies (id);
+ALTER TABLE producer
+  ADD movie_id INTEGER REFERENCES filmography.public.movies (id),
+  ADD best_movie_id INTEGER REFERENCES filmography.public.movies (id);
+
+ALTER TABLE movies
+  ALTER country SET DEFAULT 'UK';
+
+ALTER TABLE actors
+  DROP CONSTRAINT actors_number_of_movies_check;
+
+ALTER TABLE movies
+  ADD CONSTRAINT ch_min_price CHECK (budget > 1000);
+
+CREATE TABLE genres (
+  id   SERIAL PRIMARY KEY,
+  name VARCHAR(50)
+);
+
+ALTER TABLE movies
+  DROP genres;
+ALTER TABLE movies
+  ADD genre_id INTEGER REFERENCES genres (id);
+
+CREATE TYPE COUNTRY AS ENUM ('USA', 'UK', 'Russia', 'France', 'Germany');
+
+ALTER TABLE person
+  DROP motherland;
+ALTER TABLE person
+  ADD motherland COUNTRY;
+
+ALTER TABLE person
+  ADD CONSTRAINT ch_right_date CHECK (birthday <= CURRENT_DATE);
+
+CREATE INDEX ON actors (name, surname);
+
+UPDATE movies
+SET name = concat(name, '(', year, ')');
